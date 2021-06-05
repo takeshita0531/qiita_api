1905c0c47ad785dfe6747317575523dc94a61567
EC2 Default User <ec2-user@ip-172-31-25-252.us-east-2.compute.internal>
2021-06-05 16:28:34 +0000

    Add method_searches_controller code search function

--- app/controllers/method_searches_controller.rb
+++ app/controllers/method_searches_controller.rb
@@ -1,4 +1,15 @@
 class MethodSearchesController < ApplicationController
     def index
+        extracted_method_array = []
+        ObjectSpace.each_object(Class) do |class_all|
+            class_all.methods.each do |method|
+                code_all = params[:code]
+                method_result = code_all.slice(method.to_s)
+                if method_result != nil
+                   extracted_method_array.push(method_result)
+                   @extracted_methods = extracted_method_array.uniq
+                end
+            end 
+        end 
     end 
 end
