1f7c562a770adadb649616b315025ae39c3a650d
Ubuntu <ubuntu@ip-172-31-36-12.us-east-2.compute.internal>
2021-08-05 17:50:56 +0000

    Update folders_controller

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -10,21 +10,33 @@ class FoldersController < ApplicationController
   end 
   
   def index
-    query = 'created:>2015-10-09' # 参考 検索時に利用できるオプション
-    status, next_page, @items = QiitaApiManager.search(query)
+    @qiita_api = Api::QiitaApi.new
+    @qiita_api.qiita_api
     QiitaMemoryJob.delay(run_at: 1.minutes).perform_later
     @folder_new = Folder.new
     @folders = QiitaMemory.search(params[:search])
+    # # url = @qiita_api.items
+    # @qiita_url = []
+    # @qiita_api.items.each do |url|
+    #   url = Folder.find_by
+    # # folders = Folder.all
+    # # @qiita_url = Folder.select("url")
+    # #   binding.pry
+    # end
   end
   
   def create
-      query = 'created:>2015-10-09'
-      status, next_page, @items = QiitaApiManager.search(query)
+      @qiita_api = Api::QiitaApi.new
+      @qiita_api.qiita_api
+      @folder_new = Folder.new
+      #   # @qiita_api.items.each do |url|
+      #     @qiita_url = Folder.all
+        # end
       folder = current_user.folders.new(folder_params)
       respond_to do |format|
       if folder.save
         format.html  {redirect_back(fallback_location: true)}
-        format.js { render js: 'folders/create.js.erb' }
+        format.js
       else
         format.html { render :index } 
         format.js { render :errors } 
@@ -53,44 +65,6 @@ class FoldersController < ApplicationController
       render 'files/index'
     end
   end 
-  
-  
-  require 'net/http'
-  require 'json'
-  
-  
-
-  class QiitaApiManager
-    PER_PAGE = 100
-    ACCESS_TOKEN = 'f0ac82b225df283fcbeee8dbb596b6feae29db03'
-    GET_ITEMS_URI = 'https://qiita.com/api/v2/items'
-  
-    def self.search(query, page: 1)
-      puts "API Search Condition: query:'#{query}', page:#{page}"
-  
-      # リクエスト情報を作成
-      uri = URI.parse (GET_ITEMS_URI)
-      uri.query = URI.encode_www_form({ query: query, per_page: PER_PAGE, page: page })
-      req = Net::HTTP::Get.new(uri.request_uri)
-      req['Authorization'] = "Bearer #{ACCESS_TOKEN}"
-  
-      # リクエスト送信
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      res = http.request(req)
-  
-      # 次ページを計算 (API仕様 上限は100ページ)
-      total_page = ((res['total-count'].to_i - 1) / PER_PAGE) + 1
-      total_page = (total_page > 100) ? 100 : total_page
-      next_page = (page < total_page) ? page + 1 : nil
-  
-      # API 残り使用回数、リセットされる時刻を表示
-      puts "API Limit:#{res['rate-remaining']}/#{res['rate-limit']}, reset:#{Time.at(res['rate-reset'].to_i)}"
-  
-      # 返却 [HTTPステータスコード, 次ページ, 記事情報の配列]
-      return res.code.to_i, next_page, JSON.parse(res.body)
-    end
-  end
   
   private
   
